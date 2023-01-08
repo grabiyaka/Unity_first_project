3cbd81c81fd1924a38eda0520fac195fc8a808c0
grabiyaka <illya.gerasimets@gmail.com>
2023-01-08 20:50:43 +0200

    lesson 3

--- Assets/Scripts/Basics.cs
+++ Assets/Scripts/Basics.cs
@@ -6,15 +6,43 @@ using System;
 public class Basics : MonoBehaviour
 {
     
-    public string word;
-
-    private float nums;
+    
+    [SerializeField] private float nums;
 
     [NonSerialized] public int _num = 10;
 
+    public int[] numbers = new int[6];
+
+    public List<string> words = new List<string>();
+
+    public string word;
+    public int two = 2;
+    public int five = 3; //   3+2
+
+    public string nnk = "Nikitos16";
+    public bool tf = true;
+
     void Start()
     {
-        Debug.Log(word + _num);
+        Debug.Log(two);
+        Debug.Log( five);
+        five = five + 8;
+        Debug.Log(five);
+        Debug.Log(nnk);
+        nnk = "Bhdn";
+        Debug.Log(nnk);
+        
+        Debug.Log(nnk = "3+2");
+
+        Debug.Log(tf = false);
+
+        Info();
+    }
+
+    void Info()
+    {
+        if(_num == 10)
+            Debug.Log(word + _num);
     }
 
     // Update is called once per frame
